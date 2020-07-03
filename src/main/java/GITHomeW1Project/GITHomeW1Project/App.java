21ec68aa80cee925258c8d4d58bf2124af027811
Marix2020 <maricelocampo888@gmail.com>
2020-07-03 17:15:58 -0400

    homework git

---
+++ src/main/java/GITHomeW1Project/GITHomeW1Project/App.java
@@ -0,0 +1,13 @@
+package GITHomeW1Project.GITHomeW1Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
